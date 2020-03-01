d9e4fc4a5b31217aa135d9bb06bc50b73d067ff0
Patrick Kelly <p-kell@live.com>
2020-03-01 10:40:05 -0500

    Closes #29

--- Patterns/Compare.cs
+++ Patterns/Compare.cs
@@ -1,24 +1,47 @@
-﻿namespace Stringier.Patterns {
+﻿using System;
+
+namespace Stringier.Patterns {
 	/// <summary>
 	/// The comparison mode to use when parsing.
 	/// </summary>
-	public enum Compare {
+	[Flags]
+	public enum Compare : Byte {
 		/// <summary>
-		/// No comparison preference (default behavior is <see cref="CaseSensitive"/>).
+		/// No comparison preference (default behavior is <see cref="CaseSensitive"/>, <see cref="GlyphSensitive"/>).
 		/// </summary>
 		/// <remarks>
-		/// This is intended to specify "I don't care", or default behavior, for <see cref="Source"/>.
+		/// This is intended to specify "I don't care", or default behavior.
 		/// </remarks>
-		NoPreference = 0x0000_0000,
+		NoPreference = 0b0000_0000,
 
 		/// <summary>
 		/// Comparisons should be done with regards to letter casing.
 		/// </summary>
-		CaseSensitive = 0x2AAA_AAAA,
+		CaseSensitive = 0b0000_0001,
 
 		/// <summary>
 		/// Comparisons should be done without regards to letter casing.
 		/// </summary>
-		CaseInsensitive = 0x7FFF_FFFF,
+		CaseInsensitive = 0b0000_0010,
+
+		/// <summary>
+		/// Comparisons don't have a casing preference.
+		/// </summary>
+		NoCasePreference = 0b0000_0011,
+
+		/// <summary>
+		/// Comparisons should be done with regards to grapheme encoding.
+		/// </summary>
+		GlyphSensitive = 0b0001_0000,
+
+		/// <summary>
+		/// Comparisons should be done without regards to grapheme encoding.
+		/// </summary>
+		GlyphInsensitive = 0b0010_0000,
+
+		/// <summary>
+		/// Comparisons don't have a grapheme preference.
+		/// </summary>
+		NoGlyphPreference = 0b0011_0000,
 	}
 }
