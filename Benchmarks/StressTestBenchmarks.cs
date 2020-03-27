61fec54d867cf4c536c403231a450257d84fafc2
Patrick Kelly <p-kell@live.com>
2020-03-27 09:18:09 -0400

    Made StressTests run off its respective benchmark code; testing all benchmarks for that now

--- Benchmarks/StressTestBenchmarks.cs
+++ Benchmarks/StressTestBenchmarks.cs
@@ -12,23 +12,23 @@ using static Stringier.Patterns.Pattern;
 
 namespace Benchmarks {
 	[SimpleJob(RuntimeMoniker.NetCoreApp31)]
-	[SimpleJob(RuntimeMoniker.CoreRt31)]
-	[SimpleJob(RuntimeMoniker.Mono)]
 	[MemoryDiagnoser]
 	public class StressTestBenchmarks {
-		public static String goodGibberish = Gibberish.Generate(4);
+		public const Int32 ReductionFactor = 2048;
 
-		public static String badGibberish = Gibberish.Generate(4, bad: true);
+		public static String goodGibberish = Gibberish.Generate(ReductionFactor);
 
-		private static readonly Regex msregex = new Regex(@"(?:[a-z]+| +)+$", RegexOptions.Singleline);
+		public static String badGibberish = Gibberish.Generate(ReductionFactor, bad: true);
 
-		private static readonly PcreRegex pcreregex = new PcreRegex(@"(?:[a-z]+| +)+$", PcreOptions.Singleline);
+		public static readonly Regex msregex = new Regex(@"(?:[a-z]+| +)+", RegexOptions.Singleline);
 
-		private static readonly Parser<Char, String> pidgin = Parser.ManyString(Parser.Letter.Or(Parser.Char(' ')));
+		public static readonly PcreRegex pcreregex = new PcreRegex(@"(?:[a-z]+| +)+", PcreOptions.Singleline);
 
-		private static readonly Sprache.Parser<String> sprache = Parse.Letter.Or(Parse.Char(' ')).Many().Text();
+		public static readonly Parser<Char, String> pidgin = Parser.ManyString(Parser.Letter.Or(Parser.Char(' ')));
 
-		private static readonly Pattern stringer = Many(Many(Check((Char) => 'a' <= Char && Char <= 'z') | Many(' ').Then(EndOfSource)));
+		public static readonly Sprache.Parser<String> sprache = Parse.Letter.Or(Parse.Char(' ')).Many().End().Text();
+
+		public static readonly Pattern stringer = Many(Many(Check((Char) => 'a' <= Char && Char <= 'z') | Many(' '))).Then(EndOfSource);
 
 		[Benchmark]
 		public void MSRegex_Good() => msregex.Match(goodGibberish);
@@ -58,7 +58,13 @@ namespace Benchmarks {
 		public void Sprache_Good() => sprache.Parse(goodGibberish);
 
 		[Benchmark]
-		public void Sprache_Bad() => sprache.Parse(badGibberish);
+		public void Sprache_Bad() {
+			try {
+				_ = sprache.Parse(badGibberish);
+			} catch {
+				// We have to do this or benchmarks are thrown out. This is considered part of Sprache's failure mode.
+			}
+		}
 
 		[Benchmark]
 		public void Stringier_Good() => stringer.Consume(goodGibberish);
--- Tests/StressTests.cs
+++ Tests/StressTests.cs
@@ -3,17 +3,75 @@ using Stringier.Patterns;
 using static Stringier.Patterns.Pattern;
 using Defender;
 using Xunit;
+using FParsec.CSharp;
+using Pidgin;
+using Sprache;
+using Benchmarks;
+using static Benchmarks.StressTestBenchmarks;
+using System.Text.RegularExpressions;
+using PCRE;
 
 namespace Tests {
 	public class StressTests : Trial {
+
 		[Fact]
-		public void Gibberish() {
-			Source source = new Source(Stringier.Gibberish.Generate(128));
-			Pattern letter = Check((c) => 'a' <= c && c <= 'z');
-			Pattern word = Many(letter);
-			Pattern space = Many(' ');
-			Pattern gibberish = Many(word | space).Then(EndOfSource);
-			Claim.That(gibberish).SucceedsToConsume(ref source);
+		public void MSRegex_Good() {
+			Match match = msregex.Match(goodGibberish);
+			Claim.That(match.Success).Equals(true);
+		}
+
+		[Fact]
+		public void PcreRegex_Good() {
+			PcreMatch match = pcreregex.Match(goodGibberish);
+			Claim.That(match.Success).Equals(true);
+		}
+
+		[Fact]
+		public void FParsec_Good() {
+			var result = fparsec.Gibberish.RunOnString(goodGibberish);
+			Claim.That(result.IsSuccess).Equals(true);
+		}
+
+		[Fact]
+		public void FParsec_Bad() {
+			var result = fparsec.Gibberish.RunOnString(badGibberish);
+			Claim.That(result.IsSuccess).Equals(false);
+		}
+
+		[Fact]
+		public void Pidgin_Good() {
+			var result = pidgin.Parse(goodGibberish);
+			// This bizarre looking comparison is the result of Pidgin not bothering to understand end of source/stream
+			Claim.That(result.Value.Length == Int32.MaxValue / ReductionFactor).Equals(true);
+		}
+
+		[Fact]
+		public void Pidgin_Bad() {
+			var result = pidgin.Parse(badGibberish);
+			// This bizarre looking comparison is the result of Pidgin not bothering to understand end of source/stream
+			Claim.That(result.Value.Length == Int32.MaxValue / ReductionFactor).Equals(false);
+		}
+
+		[Fact]
+		public void Sprache_Good() {
+			var result = sprache.Parse(goodGibberish);
+			Claim.That(result.Length > 0).Equals(true);
+		}
+
+		[Fact]
+		public void Sprache_Bad() =>
+			Claim.That(() => sprache.Parse(badGibberish)).Throws<Sprache.ParseException>();
+
+		[Fact]
+		public void Stringier_Good() {
+			var result = stringer.Consume(goodGibberish);
+			Claim.That(result.Success).Equals(true);
+		}
+
+		[Fact]
+		public void Stringer_Bad() {
+			var result = stringer.Consume(badGibberish);
+			Claim.That(result.Success).Equals(false);
 		}
 	}
 }
